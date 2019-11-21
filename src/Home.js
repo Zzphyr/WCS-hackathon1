55e64813165aab054b36dba06c49e5b78d791f2a
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-21 12:52:10 +0000

    root done

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+        
+
+
+const Home = () => {
+    return (
+        <div></div>
+    )
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    export default Home; 
---
+++ src/Profile.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div></div>
+    )
+
+
+
+
+
+
+
+}
+
+export default Profile; 
