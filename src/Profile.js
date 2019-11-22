46c502d904e94e85f6ffb5ccfc9c9012da52725d
AngelinaRIET <angelina.riet@yahoo.com>
2019-11-22 02:55:10 +0000

    css

--- src/Profile.js
+++ src/Profile.js
@@ -12,17 +12,22 @@ const Profile = ({ chosenOne }) => {
             <div className="profile-description">
                 <div className="profile-name"> {chosenOne.name} </div>
                 <div className="profile-settings"> {chosenOne.gender} - {chosenOne.species} - {chosenOne.homeworld} </div>
-                <div className="profile-quote"> {starwars()}</div>
-                <form className="profile-form" method="post" action="traitement.php">
-                    <input type="email" name="yourEmail" value="" id="emailLabel" placeholder="youremail@email.com" required />
+                <div className="profile-quote">"{starwars()}"</div>
+                <form action="https://formspree.io/mayyzgrd" className="profile-form" method="post">
+                    <div className="profile-email">
+                        <input className="profile-email-style" type="email" name='email' id="email" placeholder="Your email" size="30" />
+                    </div>
                     <textarea className="profile-form-text-area" id="text"></textarea>
                     <div className="profile-btn">
-                        <input className="profile-btn-style" type="submit" value="Send your love" />
+                        <button className="profile-btn-style" type="submit">Send <i class="fas fa-heart"></i></button>
                     </div>
                 </form>
 
             </div>
         </div>
+
+
+
 
 
     )
